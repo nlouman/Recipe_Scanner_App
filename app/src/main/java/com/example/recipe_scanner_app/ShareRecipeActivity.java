6bf451cb186594c2a0d5102ad69e86f2389a0b1e
nlouman <nino.louman@gmail.com>
2020-11-04 09:51:56 +0100

    Scnd Commit

---
+++ app/src/main/java/com/example/recipe_scanner_app/ShareRecipeActivity.java
@@ -0,0 +1,14 @@
+package com.example.recipe_scanner_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShareRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_recipe);
+    }
+}
