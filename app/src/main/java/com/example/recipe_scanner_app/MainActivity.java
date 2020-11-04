4e3483dbbfde18a5da6f4369931c55143bab52ad
nlouman <nino.louman@gmail.com>
2020-11-04 09:25:15 +0100

    First Commit

--- app/src/main/java/com/example/recipe_scanner_app/MainActivity.java
+++ app/src/main/java/com/example/recipe_scanner_app/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int q = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
