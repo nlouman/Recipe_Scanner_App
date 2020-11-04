21c409583cee475f30769e679bf396d46a0e3f8a
Jerrzv1v2 <lawzorov2v2@gmail.com>
2020-11-04 11:20:12 +0100

    login1 commit

--- app/src/main/java/com/example/recipe_scanner_app/LoginActivity.java
+++ app/src/main/java/com/example/recipe_scanner_app/LoginActivity.java
@@ -1,14 +1,76 @@
 package com.example.recipe_scanner_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    EditText emailtext,passwordtext;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        emailtext=findViewById(R.id.textusername);
+        passwordtext=findViewById(R.id.textpassword);
+        FirebaseUser firebaseUser= mAuth.getCurrentUser();
+        if(firebaseUser!= null) {
+            Intent intent= new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+
+}
+
+
+    public void signupClicked(View view){
+        String email = emailtext.getText().toString();
+        String password= passwordtext.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Intent intent= new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);}
+        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+
+
+    public void loginClicked(View view){
+        String email = emailtext.getText().toString();
+        String password= passwordtext.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Intent intent= new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);}
+        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
     }
 }
