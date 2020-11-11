6c605d319b58cbad21975558fa18987ab904338e
Jerrzv1v2 <lawzorov2v2@gmail.com>
2020-11-11 11:01:30 +0100

    login1.1

--- app/src/main/java/com/example/recipe_scanner_app/DisplayRecipeActivity.java
+++ app/src/main/java/com/example/recipe_scanner_app/DisplayRecipeActivity.java
@@ -2,7 +2,16 @@ package com.example.recipe_scanner_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class DisplayRecipeActivity extends AppCompatActivity {
 
@@ -10,5 +19,37 @@ public class DisplayRecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_recipe);
+    }
+
+    public void stringtopdf(String data) {
+        String extstoragedir = Environment.getExternalStorageDirectory().toString();
+        File fol = new File(extstoragedir, "pdf");
+        File folder = new File(fol, "pdf");
+        if (!folder.exists()) {
+            boolean bool = folder.mkdir();
+        }
+        try {
+            final File file = new File(folder, "sample.pdf");
+            file.createNewFile();
+            FileOutputStream fOut = new FileOutputStream(file);
+
+
+            PdfDocument document = new PdfDocument();
+            PdfDocument.PageInfo pageInfo = new
+                    PdfDocument.PageInfo.Builder(100, 100, 1).create();
+            PdfDocument.Page page = document.startPage(pageInfo);
+            Canvas canvas = page.getCanvas();
+            Paint paint = new Paint();
+
+            canvas.drawText(data, 10, 10, paint);
+
+
+            document.finishPage(page);
+            document.writeTo(fOut);
+            document.close();
+
+        } catch (IOException e) {
+            Log.i("error", e.getLocalizedMessage());
+        }
     }
 }
